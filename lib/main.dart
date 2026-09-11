d9e68b9d07d8a61d533759d14ba5c9692ecfddd6
abdulsamad010 <abdulsamadabbasi010@gmail.com>
2026-09-11 04:19:35 +0300

    implemented and receved FireBase Messaging token for testing FCM

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:expenseflow_firebase_flutter/features/authentication/authentication_bloc.dart';
 import 'package:expenseflow_firebase_flutter/features/authentication/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -14,6 +15,17 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  await FirebaseMessaging.instance.requestPermission(
+    alert:true,
+    badge: true,
+    sound: true,
+  );
+
+  final token=await FirebaseMessaging.instance.getToken();
+  print("Token: $token");
+
+
   runApp(const MyApp());
 }
 

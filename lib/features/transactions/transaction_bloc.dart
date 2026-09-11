bf96d6abc221e9e92a894500f86cf7cea1161ea4
abdulsamad010 <abdulsamadabbasi010@gmail.com>
2026-09-12 04:31:34 +0500

    Add user name to transaction state

--- lib/features/transactions/transaction_bloc.dart
+++ lib/features/transactions/transaction_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:expenseflow_firebase_flutter/core/models/transaction_model.dart';
 import 'package:expenseflow_firebase_flutter/features/transactions/transaction_event.dart';
@@ -47,9 +49,13 @@ class TransactionBloc extends Bloc<TransactionEvent,TransactionState>{
 
         await FirebaseFirestore.instance.collection("users").doc("transactions").collection(FirebaseAuth.instance.currentUser!.uid).doc(event.tId).delete();
 
+
+
         emit(TransactionState(
           state: "success",
         ));
+
+        add(FetchTransaction());
 
       }
       catch(exception){
@@ -61,8 +67,12 @@ class TransactionBloc extends Bloc<TransactionEvent,TransactionState>{
 
     });
 
+
+
+
     on<FetchTransaction>((event,emit) async {
 
+      double balance=0,income=0,expense=0;
       emit(TransactionState(
         state: "sending",
       ));
@@ -72,14 +82,35 @@ class TransactionBloc extends Bloc<TransactionEvent,TransactionState>{
         final transactions1=await FirebaseFirestore.instance.collection("users").doc("transactions").collection(FirebaseAuth.instance.currentUser!.uid).get();
 
         List<TransactionModel> transactions2=[];
-        
+
         for(int i=0;i<transactions1.docs.length;i++){
           TransactionModel transactions3=TransactionModel(tId: transactions1.docs[i]["tId"], uId:transactions1.docs[i]["uId"], name: transactions1.docs[i]["name"], isExpense: transactions1.docs[i]["isExpense"], amount: (transactions1.docs[i]["amount"] as num).toDouble(), date: (transactions1.docs[i]["date"] as Timestamp).toDate() , category: transactions1.docs[i]["category"]);
           transactions2.add(transactions3);
         }
 
+        for(int i=0;i<transactions2.length;i++){
+          if(transactions2[i].isExpense==false){
+            income=income+transactions2[i].amount;
+          }
+          else{
+            expense=expense+transactions2[i].amount;
+          }
+        }
+
+        balance=income-expense;
+
+
+        final name=await FirebaseFirestore.instance.collection("user_information").doc(FirebaseAuth.instance.currentUser!.uid).get();
+
+        final email=await FirebaseAuth.instance.currentUser!.email;
+
         emit(TransactionState(
           transactions: transactions2,
+          balance: balance,
+          user_email: email!=null ? email : state.user_email,
+          income: income,
+          expense: expense,
+          user_name: name.data()?["name"],
           state: "success",
         ));
 
@@ -92,6 +123,9 @@ class TransactionBloc extends Bloc<TransactionEvent,TransactionState>{
 
 
     });
+
+
+
 
 
   }
--- lib/features/transactions/transaction_state.dart
+++ lib/features/transactions/transaction_state.dart
@@ -3,6 +3,9 @@ import 'package:expenseflow_firebase_flutter/core/models/transaction_model.dart'
 class TransactionState {
   final List<TransactionModel>? transactions;
   final String? state;
-  TransactionState({this.transactions,this.state});
+  final String user_name,user_email;
+  final double balance,income,expense;
+
+  TransactionState({this.user_email="@gmail.com",this.transactions,this.state,this.balance=0,this.expense=0,this.user_name="User",this.income=0});
 
 }
